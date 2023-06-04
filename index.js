83b7f6c56236fda7a6d1f3f596f17f263f7216e3
ukrwebprom <ukrwebprom@gmail.com>
2023-06-04 18:51:18 +0300

    initial

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const {UNMOTIVATOR_TOKEN, PORT} = process.env;
+const TeleBot = require('telebot');
+const bot = new TeleBot({
+    token:UNMOTIVATOR_TOKEN,
+    webhook: {
+      url:'https://Localhost:3000/webhook',
+      host:'0.0.0.0',
+      port:PORT
+    }
+  });
+
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.post('/webhook', (req, res) => {
+    const upd = req.body.message;
+    const chat_id = upd.chat.id;
+    const message = upd.text;
+    sendResponse(message.toLowerCase(), chat_id);
+    res.status(200).json({ success: true });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Webhook server is running on port ${PORT}`);
+  });
+
+  async function sendResponse(msg, id) {
+    if(msg.includes('нода')) {
+        await bot.sendMessage(id, 'Нода?? Ненавижу ноду');
+        return;
+    }
+  }
